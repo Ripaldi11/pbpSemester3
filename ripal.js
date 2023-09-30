788e281de8228a57f70c2e12254ed5f044bf7023
Ripaldi11 <123799290+Ripaldi11@users.noreply.github.com>
2023-09-30 21:10:16 +0700

    Add files via upload

---
+++ ripal.js
@@ -0,0 +1,10 @@
+
+let firstName = "ripal Herdiansyah" ;
+let lastName = "Muhamad" ;
+let age = 20 ;
+isMarried = age == 25;
+
+console.log(`Identitas Mahasiswa`)
+console.log(`Nama: ${lastName} ${firstName}`)
+console.log(`Umur: ${age}`)
+console.log(`Menikah: ${isMarried}`)
